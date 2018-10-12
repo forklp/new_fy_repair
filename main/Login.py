a9bf9719f939ace43f8d5ae2ed4dc2564bd18daf
forklp <1637108208@qq.com>
2018-10-12 10:36:53 +0800

    login

--- main/Login.py
+++ main/Login.py
@@ -1,41 +1,43 @@
 from django.shortcuts import HttpResponse, redirect
 import json
+import requests
+from urllib import request
+from urllib import parse
+from urllib.request import urlopen
 # Create your views here.
 def login(request):
     if request.method == 'GET':
         try:
-            user_id = request.GET['uid']
-            user_token = request.GET['token']
-            request.session['uid'] = user_id
-            request.sesssion['token'] = user_token
+            uid = request.GET['uid']
+            token = request.GET['token']
+            appid = 1011
+            appkey = 'jdf943u8uiefjwieu99e80we9'
+            data = {
+                "uid": uid,
+                "token": token,
+                "appid": appid,
+                "appkey": appkey
+            }
+            url = 'https://sso.fyscu.com/api/challenge'
+            r = requests.post(url, data=data)
+            user_info = r.json()
+            request.session['user_info'] = user_info
+            return redirect('http://132.232.107.63:3000/dashboard')
         except:
-            pass
-    return redirect('http://132.232.107.63:3000/dashboard')
+            return HttpResponse('error')
+    return HttpResponse('404')
 
 def getUserInfo(request):
     if request.method == 'POST':
-        appid = 1011
-        appkey = 'jdf943u8uiefjwieu99e80we9'
-        try:
-            uid = request.session['uid']
-            token = request.session['token']
-        except:
-            uid = ''
-            token = ''
-        data = {
-            "uid": uid,
-            "token": token,
-            "appid": appid,
-            "appkey": appkey
-        }
-        return HttpResponse(json.dumps(data), content_type='application/json')
+        if 'user_info' in request.session:
+            data = request.session['user_info']
+            return HttpResponse(json.dumps(data), content_type='application/json')
     return HttpResponse(404)
 
 def logout(request):
     if request.method == 'POST':
-        if 'uid' in request.session:
-            del request.session['uid']
-            del request.session['token']
+        if 'user_info' in request.session:
+            del request.session['user_info']
             return HttpResponse('success')
     return HttpResponse(404)
 
